1cd88687641f953ae8bcac26edddac73de3825c4
Siano77 <sianogaming28@gmail.com>
2025-08-24 14:49:49 +0100

    config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,16 @@
 require('dotenv').config()
 
-function prefix() { return process.env.PREFIX || '#' }
-function owner()  { return process.env.OWNER_NUMBER || '' }
-function botName(){ return process.env.BOT_NAME || 'SIANO-MD' }
+// Get prefix, owner number, and bot name from environment variables
+function prefix() {
+    return process.env.PREFIX || '#'
+}
+
+function owner() {
+    return process.env.OWNER_NUMBER || '2348100000000'
+}
+
+function botName() {
+    return process.env.BOT_NAME || 'SIANO-MD'
+}
 
 module.exports = { prefix, owner, botName }
